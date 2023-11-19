7c41e83fba24128860467a13a2ac2033215d94f3
Dodogari <gaori926@gmail.com>
2023-11-20 00:09:56 +0900

    add sign in func
    실습

--- index.js
+++ index.js
@@ -1,20 +1,45 @@
 import express from 'express';
 import { tempRouter } from './src/routes/temp.routes.js';
 import { response } from './config/response.js';
+import { userRouter } from './src/routes/user.route.js';
+import { specs } from './config/swagger.config.js';
+import SwaggerUi from 'swagger-ui-express';
+import dotenv from 'dotenv';
+import cors from 'cors';
 
 const app = express();
 const port = 3000;
 
+// .env 파일 사용 (환경 변수 관리)
+dotenv.config();
+
+// 단순 객체 문자열 형태로 본문 데이터 해석
+app.use(express.urlencoded({extended: false})); 
+
+// 스웨거
+app.use('/api-docs', SwaggerUi.serve, SwaggerUi.setup(specs));
+
 // 라우터 세팅
 app.use('/umc_workbook', tempRouter);
+app.use('/user', userRouter);
+app.use('/temp', tempRouter);
+
+
+// 서버 세팅
+app.set('port', process.env.PORT || 3000)   // 서버 포트 지정
+app.use(cors());                            // cors 방식 허용
+app.use(express.static('public'));          // 정적 파일 접근
+app.use(express.json());                    // request의 본문을 json으로 해석할 수 있도록 함 (JSON 형태의 요청 body를 파싱하기 위함)
 
 // 에러 핸들링
 app.use((err, req, res, next) => {
     res.locals.message = err.message;
     res.locals.error = process.env.NODE_ENV !== 'production' ? err : {}; 
+    console.log("error", err);
     res.status(err.data.status).send(response(err.data));
 });
 
+
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${app.get('port')}`);
 });
---
+++ src/routes/user.route.js
@@ -0,0 +1,8 @@
+import express from "express";
+import asyncHandler from 'express-async-handler';
+import { userSignin } from "../controllers/user.controller.js";
+
+export const userRouter = express.Router();
+
+// post 요청
+userRouter.post('/signin', asyncHandler(userSignin));
